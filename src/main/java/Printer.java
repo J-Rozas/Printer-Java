f8725ac7dfd7d2f9aa2109af2fcf861c3213898d
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-13 21:05:21 +0100

    Initial setup for class Printer

---
+++ src/main/java/Printer.java
@@ -0,0 +1,10 @@
+public class Printer {
+    private int sheetsOfPaper;
+    private int tonerVolume;
+
+    public Printer(int sheetsOfPaper, int tonerVolume) {
+        sheetsOfPaper = sheetsOfPaper;
+        tonerVolume = tonerVolume;
+    }
+    
+}
