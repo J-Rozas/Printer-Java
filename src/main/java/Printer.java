435c13e3b50bdeadc22e2d7b42c47b428017538f
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-13 21:11:17 +0100

    Add method to print copies

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -2,9 +2,9 @@ public class Printer {
     private int sheetsOfPaper;
     private int tonerVolume;
 
-    public Printer(int sheetsOfPaper, int tonerVolume) {
-        sheetsOfPaper = sheetsOfPaper;
-        tonerVolume = tonerVolume;
+    public Printer(int numberOfSheets, int volumeOfToner) {
+        sheetsOfPaper = numberOfSheets;
+        tonerVolume = volumeOfToner;
     }
 
     public int getSheetsOfPaper() {
@@ -13,5 +13,17 @@ public class Printer {
 
     public int getTonerVolume() {
         return tonerVolume;
+    }
+
+    public String print(int copies, int pages) {
+        int totalCopies = copies * pages;
+
+        if (totalCopies > sheetsOfPaper) {
+            sheetsOfPaper -= totalCopies;
+            tonerVolume -= totalCopies;
+            return "Printed " + totalCopies + " sheets of paper";
+        } else {
+            return "Not enough paper to print that";
+        }
     }
 }
